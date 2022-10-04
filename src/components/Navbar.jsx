99897e17a489db3d9822ead98d5781a80ee8e3a3
Victor Rajkumar <victorrajkumar1@gmail.com>
2022-10-04 19:04:46 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <header className="bg-white text-gray-600 body-font shadow-md">
       <div className="container mx-auto flex flex-wrap p-3  flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center  text-gray-900 mb-0  md:mb-0">
+        <button className="flex title-font font-medium items-center  text-gray-900 mb-0  md:mb-0">
           <img className="h-10" src={Logo} alt="Logo" />
-        </a>
+        </button>
       </div>
     </header>
   );
